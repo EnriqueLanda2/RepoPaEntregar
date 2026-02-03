328977777c680cecff897f2266955c23413b3734
Enrique Landa <20233tn174@utez.edu.mx>
2026-02-03 17:56:44 -0600

    [ADD] CAMBIOS DE BUENAS PRACTICAS REALIZADOS

---
+++ env.js
@@ -0,0 +1,5 @@
+// Configuración de variables de entorno
+// Estas deberían ser cargadas desde un archivo .env en producción
+
+export const API_KEY = 'sk_12345abcdef67823GHIJKLMNYU';
+export const SESSION_TOKEN = "TOKEN_" + Math.random().toString(36).substring(7);
