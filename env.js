f6f8e42b5a20713ba20bb528579b8096a8f8c54a
EnriqueLanda2 <20233TN174@utez.edu.mx>
2026-02-03 18:05:01 -0600

    Delete env.js SORRI

--- env.js
+++
@@ -1,5 +0,0 @@
-// Configuración de variables de entorno
-// Estas deberían ser cargadas desde un archivo .env en producción
-
-export const API_KEY = 'sk_12345abcdef67823GHIJKLMNYU';
-export const SESSION_TOKEN = "TOKEN_" + Math.random().toString(36).substring(7);
